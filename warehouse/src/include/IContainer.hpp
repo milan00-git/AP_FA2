e877743eaa4e181c091e7d43e6dc6def163f3ac3
Milan <mp09092003@gmail.com>
2023-05-26 18:27:17 +0200

    documentation added

--- warehouse/src/include/IContainer.hpp
+++ warehouse/src/include/IContainer.hpp
@@ -1,8 +1,9 @@
 #pragma once 
 
+//abstract class
 class IContainer 
 {
     public:
-        virtual bool isEmpty() = 0;
+        virtual bool isEmpty() = 0; 
         virtual bool isFull() = 0;
 };
